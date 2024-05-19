64a590a74e214a104ba27104a2b85baa98f9df64
yudai.usuda <yudai.usuda@zozo.com>
2024-05-19 21:55:52 +0900

    feat: Shorten Amazon URL with bookmarklet
    This commit adds a bookmarklet to shorten Amazon URLs. The bookmarklet is a JavaScript function that can be executed in the browser's address bar. It retrieves the ASIN (Amazon Standard Identification Number) from the page and redirects the user to the shortened URL on Amazon.jp.
    
    Ref: shorten-amazon-url.js

--- shorten-amazon-url.js
+++ shorten-amazon-url.js
@@ -1,2 +1,9 @@
+/**
+ * Shorten Amazon URL
+ *
+ * javascript:(function(url){s=document.createElement('script');s.src=url;document.body.appendChild(s);})('https://udus122.github.io/bookmarklet/shorten-amazon-url.js');
+ *
+ * <p>Amazon の商品ページの URL を短縮する。</p>
+ */
 const asin = document.getElementById("ASIN").value;
 location.href = `http://amazon.jp/dp/${asin}`;
