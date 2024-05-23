9c29d18a5550acf5911cdf53f13e9e6070819e6d
yudai.usuda <yudai.usuda@zozo.com>
2024-05-23 09:49:14 +0900

    feat: Add bookmarklet to open Obsidian from UDLog
    This commit adds a bookmarklet that allows users to open Obsidian with a specific note from the UDLog website. The bookmarklet is a JavaScript function that constructs the Obsidian URL based on the current page's URL and opens it in a new tab. This feature enhances the integration between UDLog and Obsidian, providing a seamless transition for users.

--- open-obsidian-from-udlog.js
+++ open-obsidian-from-udlog.js
@@ -1,3 +1,6 @@
+/**
+ * javascript:(function(url){s=document.createElement('script');s.src=url;document.body.appendChild(s);})('https://udus122.github.io/bookmarklet/open-obsidian-from-udlog.js');
+ */
 window.open(
   "obsidian://open?vault=notes&file=" +
     location.href.replace("https://notes.udus.dev/", "").replace(/\+/g, " ")
