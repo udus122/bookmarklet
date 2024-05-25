bf2bcd8d9778c803ac6055e6285e8bbf2af5eeef
yudai.usuda <yudai.usuda@zozo.com>
2024-05-25 10:56:12 +0900

    feat: Improve book information extraction and add book notes template
    This commit improves the extract-book-info-from-amazon.js file by updating the code's formatting and structure. It also enhances the book information extraction process and includes a template for book notes. These changes improve the readability, maintainability, and usability of the code.
    
    Refactor the code to improve readability and maintainability. Update variable names, remove unnecessary comments, and improve code formatting and structure. Add a template for book notes and a checklist for recording reading progress. Update the book cover image URL retrieval process.

--- extract-book-info-from-amazon.js
+++ extract-book-info-from-amazon.js
@@ -1,43 +1,124 @@
 // 書籍名の取得
-const productTitle = document.getElementById("productTitle");
-const ebooksProductTitle = document.getElementById("ebooksProductTitle");
-const title = productTitle
-  ? productTitle.innerText.trim()
-  : ebooksProductTitle.innerText.trim();
+function getTitleEl() {
+  // 通常、このIDで書籍のタイトルを取得できるはず
+  const productTitleEl = document.getElementById("productTitle");
+  if (productTitleEl) {
+    return productTitleEl?.innerText.trim() ?? "";
+  }
+
+  //
+  const ebooksProductTitleEl = document.getElementById("ebooksProductTitle");
+  if (ebooksProductTitleEl) {
+    return ebooksProductTitleEl?.innerText.trim() ?? "";
+  }
+
+  const ebooksTitleEl = document.getElementById("ebooksTitle"); // mobile
+  if (ebooksTitleEl) {
+    return ebooksTitleEl?.innerText.trim() ?? "";
+  }
+
+  const titleEl = document.getElementById("title");
+  return titleEl?.innerText.trim() ?? "";
+}
+
+const title = getTitleEl();
 
 // ASIN の取得
-const asinId = document.getElementById("ASIN");
-const asin = asinId
-  ? asinId.value
-  : document.getElementsByName("ASIN.0")[0].value;
+function getASIN() {
+  const ASINIdEl = document.getElementById("ASIN");
+  if (ASINIdEl) {
+    return ASINIdEl.value;
+  }
+  const asin_0El = document.getElementsByName("ASIN.0")[0];
+  if (asin_0El) {
+    return asin_0El.value;
+  }
+  const asinIdEl = document.getElementById("asin");
+  if (asinIdEl) {
+    return asinIdEl.value;
+  }
+}
+const asin = getASIN();
+console.log(asin);
 
 // 書影の取得
-const imageurl = document.getElementById("landingImage").getAttribute("src");
+function getImageUrl() {
+  // desktop
+  const landingImageEl = document.getElementById("landingImage");
+  if (landingImageEl) {
+    return landingImageEl?.getAttribute("src") ?? "";
+  }
+  // 通常書籍 mobile
+  const mainImageEl = document.getElementById("main-image");
+  if (mainImageEl) {
+    return mainImageEl?.getAttribute("src") ?? "";
+  }
+  // Kindle mobile
+  const ebooksImgBlkFrontEl = document.getElementById("ebooksImgBlkFront");
+  if (ebooksImgBlkFrontEl) {
+    return ebooksImgBlkFrontEl?.getAttribute("src") ?? "";
+  }
+}
+
+const imageurl = getImageUrl();
 
 // 著者情報の取得
-const authors = [...document.querySelectorAll(".author")].map((c) => {
-  return c.innerText
-    .replace(/\r?\n/g, "")
-    .replace(/,/, "")
-    .replace(/\(.+\)/, "")
-    .replace(/ /g, "");
-});
+function getAuthors() {
+  const author_alinknormalEls = document.querySelectorAll(
+    ".author .a-link-normal"
+  );
+  if (author_alinknormalEls.length > 0) {
+    return Array.from(author_alinknormalEls).map((el) => el.innerText.trim());
+  }
+  const authorSection_authorNameEls = document.querySelectorAll(
+    ".authorSection .authorName"
+  );
+  if (authorSection_authorNameEls.length > 0) {
+    return Array.from(authorSection_authorNameEls).map((el) =>
+      el.innerText.trim()
+    );
+  }
+  return [];
+}
+
+const authors = getAuthors();
 
 //登録情報欄を取得
-let detail = document.getElementById("detailBullets_feature_div");
-if (!detail) {
-  const subdoc = document.getElementById("product-description-iframe")
-    .contentWindow.document;
-  detail = subdoc.getElementById("detailBullets_feature_div");
+function getDetailsList() {
+  const detailBullets_feature_div = document.getElementById(
+    "detailBullets_feature_div"
+  );
+  if (detailBullets_feature_div) {
+    return detailBullets_feature_div.innerText.split("\n");
+  }
+  const product_description_iframe = document
+    .getElementById("product-description-iframe")
+    ?.contentWindow.document?.contentWindow.document?.getElementById(
+      "detailBullets_feature_div"
+    );
+  if (product_description_iframe) {
+    return product_description_iframe.innerText.split("\n");
+  }
+
+  const productDetails_techSpec_section_1 = document.querySelectorAll(
+    "#productDetails_techSpec_section_1 tr"
+  );
+  if (productDetails_techSpec_section_1.length > 0) {
+    return Array.from(productDetails_techSpec_section_1).map((el) =>
+      el.innerText.trim()
+    );
+  }
+  return [];
 }
+
+const detailsList = getDetailsList();
 
 // 出版社情報の取得->以降の情報取得に使う
 const publisher =
   document.querySelector(
     "#rpi-attribute-book_details-publisher > div.a-section.a-spacing-none.a-text-center.rpi-attribute-value > span"
   )?.innerText ??
-  detail.innerText
-    .split("\n")
+  detailsList
     .find((t) => t.startsWith("出版社"))
     .match(/:[\s\t　]*(.+)[\s\t　]*\(.+\)/)[1]
     .trim();
@@ -47,8 +128,7 @@ const publish_date =
   document.querySelector(
     "#rpi-attribute-book_details-publication_date > div.a-section.a-spacing-none.a-text-center.rpi-attribute-value > span"
   ).innerText ??
-  detail.innerText
-    .split("\n")
+  detailsList
     .find((t) => t.startsWith("発売日"))
     ?.match(/:[\s\t　]*(.+)/)[1]
     ?.trim();
@@ -58,13 +138,11 @@ const isbn =
   document.querySelector(
     "#rpi-attribute-book_details-isbn10 > div.a-section.a-spacing-none.a-text-center.rpi-attribute-value > span"
   )?.innerText ??
-  detail.innerText
-    .split("\n")
+  detailsList
     .find((t) => t.startsWith("ISBN-10"))
     ?.match(/:[\s\t　]*(.+)/)[1]
     ?.trim() ??
-  detail.innerText
-    .split("\n")
+  detailsList
     .find((t) => t.startsWith("ISBN-13"))
     ?.match(/:[\s\t　]*(.+)/)[1]
     ?.trim();
@@ -100,9 +178,14 @@ aliases:
 ## 読書メモ
 `;
 
-document.getElementById(
-  "bookDescription_feature_div"
-).innerHTML = `<textarea style="height:500px">${lines}</textarea>`;
+const insertElement = document.createElement("div");
+insertElement.innerHTML = `<textarea style="height:500px;border-width:0.5rem;border-style:solid">${lines}</textarea>`;
+
+const insertArea =
+  document.getElementById("bookDescription_feature_div") ??
+  document.getElementById("image-block") ??
+  document.body;
+insertArea.prepend(insertElement);
 
 function copyToClipboard(text) {
   const dummy = document.createElement("textarea");
